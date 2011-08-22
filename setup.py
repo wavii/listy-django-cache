f45f2e9d121b4e7861dd0e7107667766bf7f271d
Guido Bartolucci <guido@wavii.com>
2011-08-22 11:09:37 -0700

    Updated url.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description='Listy is a deterministic caching mechanism for django projects. It will attempt to keep the cache in-sync with the database by updating during changes instead of relying on timeouts. As implied by the name, Listy supports looking up lists of objects.',
     author='Wavii, Inc.',
     author_email='info@wavii.com',
-    url='https://github.com/wavii/django-list-cache/',
+    url='https://github.com/wavii/listy-django-cache',
     packages=['listy'],
     install_requires=['Django>=1.2, <1.3', 'python-dateutil>=1.5', 'python-memcached>=1.45'],
     test_suite='tests.run',
